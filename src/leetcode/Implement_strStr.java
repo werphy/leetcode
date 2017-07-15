aaaf6229666ecbe36508242ba2a3bde2a1405dd6
werphy <364957048@qq.com>
2017-07-15 19:25:50 +0800

    7.15

---
+++ src/leetcode/Implement_strStr.java
@@ -0,0 +1,9 @@
+package leetcode;
+
+public class Implement_strStr {
+	public class Solution {
+	    public int strStr(String haystack, String needle) {
+	       return haystack.indexOf(needle);
+	    }
+	}
+}
