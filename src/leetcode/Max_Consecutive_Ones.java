18d249a0e553ab421927a7df83a0f39f157fba9e
werphy <364957048@qq.com>
2017-07-22 00:11:57 +0800

    7.22

--- src/leetcode/Max_Consecutive_Ones.java
+++ src/leetcode/Max_Consecutive_Ones.java
@@ -1,5 +1,27 @@
 package leetcode;
 
 public class Max_Consecutive_Ones {
-
+	public class Solution {
+	    public int findMaxConsecutiveOnes(int[] nums) {
+	        int n = 0;
+	        int m = 0;
+	        int num = 0;
+	        if(nums == null)
+	        return n;
+	        
+	        for(int i =0 ; i<nums.length; i++){
+	            if(nums[i] == 1){
+	                n++;
+	            }
+	            else if(nums[i] == 0){
+	                if(m < n){
+	                    m = n;
+	                }   
+	                n = 0;
+	            }
+	            
+	        }
+	        return m > n ? m : n;
+	    }
+	}
 }
