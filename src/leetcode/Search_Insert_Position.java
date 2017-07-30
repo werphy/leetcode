c639f625e69b25699ef008d1032194a3b6596ae6
werphy <364957048@qq.com>
2017-07-30 22:34:38 +0800

    7.30

---
+++ src/leetcode/Search_Insert_Position.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+public class Search_Insert_Position {
+	/**
+	 * Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+		You may assume no duplicates in the array.
+		
+		Here are few examples.
+		[1,3,5,6], 5 ¡ú 2
+		[1,3,5,6], 2 ¡ú 1
+		[1,3,5,6], 7 ¡ú 4
+		[1,3,5,6], 0 ¡ú 0
+	 * @author werphy
+	 *
+	 */
+	public class Solution {
+	    public int searchInsert(int[] nums, int target) {
+	        if(target <= nums[0]){
+	            return 0;
+	        }
+	        if(target > nums[nums.length-1]){
+	            return nums.length;
+	        } 
+	        for (int i=1 ; i<nums.length; i++){
+	            if(target <= nums[i] && target > nums[i-1])
+	                return i;
+	        }
+
+	         return 0;
+	    }
+	}
+}
